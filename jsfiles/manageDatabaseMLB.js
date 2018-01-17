ea839881aa0438c0070da22119e5bf7a0e65beaf
Aaron Peressini <steinaua@flip1.engr.oregonstate.edu>
2018-01-16 23:14:09 -0800

    Added MLB Datatbase Project source files

---
+++ jsfiles/manageDatabaseMLB.js
@@ -0,0 +1,187 @@
+
+
+document.addEventListener('DOMContentLoaded', bindButtons);
+
+document.getElementById('body').style.fontFamily = 'Arial';
+var h1 = document.getElementsByTagName('h1');
+var h2 = document.getElementsByTagName('h2');
+var tables = document.getElementsByTagName('table');
+var headers = document.getElementsByTagName('th');
+var tds = document.getElementsByTagName('td');
+var dobs = document.getElementsByName('dob');
+var fieldsets = document.getElementsByTagName('fieldset');
+var legends = document.getElementsByTagName('legend');
+
+
+h1[0].style.color = 'royalblue';
+
+for(var x = 0; x < h2.length; x++)
+{
+   h2[x].style.color = 'silver';
+   h2[x].style.backgroundPosition = 'top left';
+   h2[x].style.backgroundColor = 'teal';
+   h2[x].style.backgroundSize = '1px 1px';
+
+}
+
+for(var x = 0; x < tables.length; x++)
+{
+    tables[x].style.border = 'medium solid teal';
+    tables[x].style.textAlign = 'left';
+    //tables[x].style.margin = '0 auto';
+    //tables[x].style.width = '50%';
+
+}
+
+for(var x = 0; x < headers.length; x++)
+{
+    headers[x].style.textDecoration = 'underline';
+    headers[x].style.padding = '5px';
+    
+}
+
+for(var x = 0; x < tds.length; x++)
+{
+   tds[x].style.padding = '5px';
+}
+
+for(var x = 0; x < dobs.length; x++)
+{
+    var str = dobs[x].innerText.toString();
+    console.log(str);
+    dobs[x].innerText = str.substring(4,15);
+}
+
+for(var x = 0; x < fieldsets.length; x++)
+{
+   fieldsets[x].style.width = '50%';
+   fieldsets[x].style.backgroundColor = 'silver';
+
+}
+
+for(var x = 0; x < legends.length; x++)
+{
+   legends[x].style.color = 'silver';
+   legends[x].style.backgroundColor = 'teal';
+   legends[x].style.fontWeight = 'bold';
+}
+
+
+
+
+function bindButtons(){
+
+        document.getElementById('add_location_submit').addEventListener('click', function(event){
+          
+          var req = new XMLHttpRequest();
+          var city = document.getElementById('add_location_city').value;
+          if(city == "")
+          {
+              alert("City is required. Please try again.");
+              //event.preventDefault();
+              return;
+          }
+          var state = document.getElementById('add_location_state').value;
+          var country = document.getElementById('add_location_country').value;
+         // var params = "city=" + city + "&state=" + state + "&country=" + country; //"city=" + city + "&state=" + state + "&country=" + country;
+          //alert(params);
+
+          
+          req.open('POST', "/insert-location", true);
+          req.setRequestHeader('Content-Type', 'application/json');
+          req.addEventListener("load", function(){
+          if(req.status >= 200 && req.status < 400)
+          {
+              var response = req.responseText;
+              //alert(response);
+
+          }
+          else
+          {
+             console.log("Error in network request: " + req.statusText);
+          }
+              
+                          
+         });
+          //event.preventDefault();  
+          req.send('{"city": "' + city + '", "state": "' + state + '", "country": "' + country + '"}');           //'{"city": "' + city + '", "state": "' + state + '", "country": "' + country + '"}'
+                    
+        });
+
+
+
+        document.getElementById('add_team_submit').addEventListener('click', function(event){
+          
+          var req = new XMLHttpRequest();
+          var team_name = document.getElementById('add_team_name').value;
+          var stadium_name = document.getElementById('add_team_stadium_name').value;
+          var league = document.getElementById('add_team_league').value;
+          var division = document.getElementById('add_team_division').value; 
+          var wins = document.getElementById('add_team_wins').value;    
+          var losses = document.getElementById('add_team_losses').value;  
+          var mascot = document.getElementById('add_team_mascot').value;   
+          var city = document.getElementById('add_team_city').value;
+          if(city == "" || team_name == "" || stadium_name == "" || wins == "" || losses == "" || mascot == "")
+          {
+              console.log("All team attributes are required. Please try again.");
+              //event.preventDefault();
+              return;
+          }
+          var state = document.getElementById('add_team_state').value;
+          var country = document.getElementById('add_team_country').value;
+          //var params = "city=" + city + "&state=" + state + "&country=" + country; //"city=" + city + "&state=" + state + "&country=" + country;
+          //alert(params);
+
+          
+          req.open('POST', "/insert-team", true);
+          req.setRequestHeader('Content-Type', 'application/json');
+          req.addEventListener("load", function(){
+          if(req.status >= 200 && req.status < 400)
+          {
+              var response = req.responseText;
+              //alert(response);
+
+          }
+          else
+          {
+             console.log("Error in network request: " + req.statusText);
+          }
+              
+                          
+         });
+          //event.preventDefault();  
+          req.send('{"team_name": "' + team_name + '", "stadium_name": "' + stadium_name + '", "league": "' + league + '", "division": "' + division + '", "team_wins": ' + wins + ', "team_losses": ' + losses + ', "mascot": "' + mascot + '", "city": "' + city + '", "state": "' + state + '", "country": "' + country + '"}');           //'{"city": "' + city + '", "state": "' + state + '", "country": "' + country + '"}'
+                    
+        });
+
+
+        document.getElementById('teams_sort_submit').addEventListener('click', function(event){
+          
+          var req = new XMLHttpRequest();
+          
+          var order_by = document.getElementById('teams_sort_on').value;
+          //var params = "city=" + city + "&state=" + state + "&country=" + country; //"city=" + city + "&state=" + state + "&country=" + country;
+          //alert(params);
+          //alert(order_by);
+          
+          req.open('GET', "http://flip1.engr.oregonstate.edu:5100/?teams_order_by=" + order_by, true);
+          req.setRequestHeader('Content-Type', 'application/json');
+          req.addEventListener("load", function(){
+          if(req.status >= 200 && req.status < 400)
+          {
+              var response = req.responseText;
+              //alert(response);
+
+          }
+          else
+          {
+             console.log("Error in network request: " + req.statusText);
+          }
+              
+                          
+         });
+          //event.preventDefault();  
+          req.send(null);           //'{"city": "' + city + '", "state": "' + state + '", "country": "' + country + '"}'
+                    //'{"order_by": "' + order_by + '"}'
+        });
+}
---
+++ queriesDatabaseMLB.js
@@ -0,0 +1,436 @@
+
+
+var express = require('express');
+var mysql = require('./dbcon.js');
+var bodyParser = require('body-parser');
+var async = require('async');
+
+var app = express();
+var handlebars = require('express-handlebars').create({defaultLayout:'main'});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json())
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', 5100);
+app.use(express.static('jsfiles'));
+
+app.get('/',function(req,res,next){
+  var context = {};
+
+  mysql.pool.query('SELECT city, state, country FROM location', function(err, rows, fields){
+    if(err){
+      next(err);
+      return;
+    }
+    context.locations = rows;
+    //console.log(context.locations);
+    //res.render('homeMLB', context);
+  //});  
+    if(req.query.teams_order_by == null)
+    {
+        req.query.teams_order_by = 'team_id'
+    } 
+    mysql.pool.query("SELECT T.team_id, team_location_id, team_name, stadium_name, league, division, wins, losses, mascot, city, state, country FROM team T INNER JOIN location LO on LO.location_id = T.team_location_id ORDER BY " + req.query.teams_order_by, function(err, rows, fields){   // [req.query.order_by]
+        if(err){
+        next(err);
+        return;
+        }
+        context.teams = rows;
+    // console.log(context.teams);
+        //res.render('homeMLB', context);
+    //});  
+
+        mysql.pool.query('SELECT player_id, first_name, last_name, dob, position, uni_num, bats, throws, team_name, city, state, country FROM player P INNER JOIN location L on L.location_id = P.hometown_id INNER JOIN team TE on TE.team_id = P.team_id', function(err, rows, fields){
+            if(err){
+            next(err);
+            return;
+            }
+            context.players = rows;
+            //console.log(context.players);
+            //res.render('homeMLB', context);
+        //});
+
+            mysql.pool.query('SELECT first_name, last_name, batting_average, hits, runs_scored, runs_batted_in, home_runs, walks, strikeouts FROM player PL INNER JOIN hitting_stats HS on HS.player_id = PL.player_id', function(err, rows, fields){
+                if(err){
+                next(err);
+                return;
+                }
+                context.hittingstats = rows;
+                //console.log(context.hittingstats);
+                //res.render('homeMLB', context);
+            // }); 
+
+                mysql.pool.query('SELECT first_name, last_name, earned_run_average, opp_batting_average, wins, losses, walks, strikeouts FROM player PL INNER JOIN pitching_stats PS on PS.player_id = PL.player_id', function(err, rows, fields){
+                    if(err){
+                    next(err);
+                    return;
+                    }
+                    context.pitchingstats = rows;
+                    //console.log(context.hittingstats);
+                    //res.render('homeMLB', context);
+                // }); 
+
+                    mysql.pool.query('SELECT award_id, award_name FROM award', function(err, rows, fields){
+                        if(err){
+                        next(err);
+                        return;
+                        }
+                        context.awards = rows;
+                        //console.log(context);
+                        //res.render('homeMLB', context);
+
+                        mysql.pool.query('SELECT PA.player_id, PA.award_id, first_name, last_name, award_name, league, year FROM award A INNER JOIN player_awards PA on A.award_id = PA.award_id INNER JOIN player P on P.player_id = PA.player_id ORDER BY year', function(err, rows, fields){
+                            if(err){
+                            next(err);   
+                            return;
+                            }
+                            context.playerawards = rows;
+                            //console.log(context);
+                            //res.render('homeMLB', context);
+
+                            mysql.pool.query("SELECT player_id, first_name, last_name FROM player WHERE position='P'", function(err, rows, fields){
+                                if(err){
+                                next(err);
+                                return;
+                                }
+                                context.pitchers = rows;
+                                res.render('homeMLB', context);
+                            });
+                        }); 
+                    }); 
+                });   
+             });
+         });
+      });
+   }); 
+});
+
+app.post('/insert-location',function(req,res,next){
+  var context = {};
+  //console.log(req.body);
+
+  mysql.pool.query('INSERT INTO location (`city`, `state`, `country`) VALUES (?, ?, ?)', [req.body.city, req.body.state, req.body.country], function(err, result){
+    if(err){
+        if(err.code == 'ER_DUP_ENTRY'){
+            console.log(err.code);
+            console.log(req.body.city + ", " + req.body.state + " " + req.body.country + " already exists in the database.");
+        }
+         else{
+            next(err);
+            return;
+        }          
+    }
+    context.city = req.body.city;
+    context.state = req.body.state;
+    context.country = req.body.country;
+    res.send(context);
+    //res.redirect('/');
+  });  
+});
+
+app.post('/insert-team',function(req,res,next){
+  var context = {};
+  //console.log(req.body);
+  mysql.pool.query('INSERT INTO location (`city`, `state`, `country`) VALUES (?, ?, ?)', [req.body.city, req.body.state, req.body.country], function(err, results){
+    if(err){
+        if(err.code == 'ER_DUP_ENTRY'){
+            mysql.pool.query("INSERT INTO team (`team_name`, `stadium_name`, `league`, `division`, `wins`, `losses`, `mascot`, `team_location_id`) VALUES ('" + req.body.team_name + "','" + req.body.stadium_name + "','" + req.body.league + "','" + req.body.division + "'," + req.body.team_wins + "," + req.body.team_losses + ",'" + req.body.mascot + "', (SELECT location_id FROM location WHERE city='" + req.body.city + "' AND state='" + req.body.state + "' AND country='" + req.body.country + "'))", function(err, result){
+                if(err){
+                    if(err.code == 'ER_DUP_ENTRY'){
+                        console.log(err.code);
+                        console.log(req.body.team_name + " already exists in database.")
+                    }
+                    else{
+                        next(err);
+                        return;
+                    }          
+                }
+                context.team_name = req.body.team_name;
+                context.stadium_name = req.body.stadium_name;
+                context.league = req.body.league;
+                context.division = req.body.division;
+                context.team_wins = req.body.team_wins;
+                context.team_losses = req.body.team_losses;
+                context.mascot = req.body.mascot;
+                context.city = req.body.city;
+                context.state = req.body.state;
+                context.country = req.body.country;
+                res.send(context);
+                //res.redirect('/');
+            });
+        }
+         else{
+            next(err);
+            return;
+        }          
+    }
+
+    else{
+        mysql.pool.query("INSERT INTO team (`team_name`, `stadium_name`, `league`, `division`, `wins`, `losses`, `mascot`, `team_location_id`) VALUES ('" + req.body.team_name + "','" + req.body.stadium_name + "','" + req.body.league + "','" + req.body.division + "'," + req.body.team_wins + "," + req.body.team_losses + ",'" + req.body.mascot + "', (SELECT location_id FROM location WHERE city='" + req.body.city + "' AND state='" + req.body.state + "' AND country='" + req.body.country + "'))", function(err, result){
+                if(err){
+                    if(err.code == 'ER_DUP_ENTRY'){
+                        console.log(err.code);
+                        console.log(req.body.team_name + " already exists in database.")
+                    }
+                    else{
+                        next(err);
+                        return;
+                    }          
+                }
+                context.team_name = req.body.team_name;
+                context.stadium_name = req.body.stadium_name;
+                context.league = req.body.league;
+                context.division = req.body.division;
+                context.team_wins = req.body.team_wins;
+                context.team_losses = req.body.team_losses;
+                context.mascot = req.body.mascot;
+                context.city = req.body.city;
+                context.state = req.body.state;
+                context.country = req.body.country;
+                res.send(context);
+                //res.redirect('/');
+            });
+    }
+  });  
+
+ /* mysql.pool.query('INSERT INTO team (`team_name`, `stadium_name`, `league`, `division` `wins`, `losses`, `mascot`) VALUES (?, ?, ?, ?, ?, ?, ?); INSERT INTO location (`city`, `state`, `country`) VALUES (?, ?, ?); COMMIT' [req.body.team_name, req.body.stadium_name, req.body.league, req.body.division, req.body.team_wins, req.body.team_losses, req.body.mascot, req.body.city, req.body.state, req.body.country],  function(err, result){
+      console.log(req.body.city);
+    if(err){
+        if(err.code == 'ER_DUP_ENTRY'){
+            console.log(err.code);
+        }
+         else{
+            next(err);
+            return;
+        }          
+    }
+    res.redirect('/');
+  });  */
+});   
+
+
+app.post('/insert-player',function(req,res,next){
+  var context = {};
+  if(req.body.city == "" || req.body.first_name == "" || req.body.last_name == "" || req.body.dob == "" || req.body.uni_num == "")
+  {
+      console.log("All fields are required. Please try again.")
+      res.redirect('/');
+  }
+
+  else{
+      mysql.pool.query('INSERT INTO location (`city`, `state`, `country`) VALUES (?, ?, ?)', [req.body.city, req.body.state, req.body.country], function(err, results){
+    if(err){
+        if(err.code == 'ER_DUP_ENTRY'){
+            mysql.pool.query("INSERT INTO player (`first_name`, `last_name`, `dob`, `position`, `uni_num`, `bats`, `throws`, `team_id`, `hometown_id`) VALUES ('" + req.body.first_name + "','" + req.body.last_name + "','" + req.body.dob + "','" + req.body.position + "','" + req.body.uni_num + "','" + req.body.bats + "','" + req.body.throws + "','" + req.body.team_id + "', (SELECT location_id FROM location WHERE city='" + req.body.city + "' AND state='" + req.body.state + "' AND country='" + req.body.country + "'))", function(err, result){
+                console.log(req.body.team_id);
+                if(err){
+                    if(err.code == 'ER_DUP_ENTRY'){
+                        console.log(err.code);
+                    }
+                    else{
+                        next(err);
+                        return;
+                    }          
+                }
+                res.redirect('/');
+            });
+        }
+         else{
+            next(err);
+            return;
+        }          
+    }
+
+    else{
+        mysql.pool.query("INSERT INTO player (`first_name`, `last_name`, `dob`, `position`, `uni_num`, `bats`, `throws`, `team_id`, `hometown_id`) VALUES ('" + req.body.first_name + "','" + req.body.last_name + "','" + req.body.dob + "','" + req.body.position + "','" + req.body.uni_num + "','" + req.body.bats + "','" + req.body.throws + "','" + req.body.team_id + "', (SELECT location_id FROM location WHERE city='" + req.body.city + "' AND state='" + req.body.state + "' AND country='" + req.body.country + "'))", function(err, result){
+                //console.log(req.body.team_id);
+                if(err){
+                    if(err.code == 'ER_DUP_ENTRY'){
+                        console.log(err.code);
+                    }
+                    else{
+                        next(err);
+                        return;
+                    }          
+                }
+                res.redirect('/');
+            });
+    }
+  });  
+
+  }
+});
+
+app.post('/insert-award',function(req,res,next){
+  var context = {};
+  if(req.body.award_name == "")
+  {
+      console.log('Award Name is required. Please try again.');
+      res.redirect('/');
+  }
+
+else{
+  mysql.pool.query('INSERT INTO award (`award_name`) VALUES (?)', [req.body.award_name], function(err, results){
+    if(err){
+        if(err.code == 'ER_DUP_ENTRY'){
+            console.log(err.code);
+        }
+        else{
+          next(err);
+          return;
+        }          
+    }
+
+     res.redirect('/');
+    });
+}
+});  
+
+app.post('/insert-player-award',function(req,res,next){
+  var context = {};
+  //console.log(req.body.player_id, req.body.award_id, req.body.year, req.body.league);
+  mysql.pool.query('INSERT INTO player_awards (`player_id`, `award_id`, `year`, `league`) VALUES (?, ?, ?, ?)', [req.body.player_id, req.body.award_id, req.body.year, req.body.league], function(err, results){
+    if(err){
+        if(err.code == 'ER_DUP_ENTRY'){
+            console.log(err.code);
+        }
+        else{
+          next(err);
+          return;
+        }          
+    }
+
+     res.redirect('/');
+    });
+});  
+
+app.post('/delete-player-award',function(req,res,next){
+  var context = {};
+  //console.log(req.body.player_id, req.body.award_id, req.body.league, req.body.year);
+  mysql.pool.query('DELETE FROM player_awards WHERE player_id=? AND award_id=? AND league=? AND year=?', [req.body.player_id, req.body.award_id, req.body.league, req.body.year], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    
+    res.redirect('/');
+  });
+});
+
+
+app.post('/update-hitting-stats',function(req,res,next){
+  var context = {};
+
+  mysql.pool.query('INSERT INTO hitting_stats (`player_id`, `hits`, `batting_average`, `runs_scored`, `runs_batted_in`, `home_runs`, `walks`, `strikeouts`) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', [req.body.player_id, req.body.hits, req.body.batting_average, req.body.runs_scored, req.body.runs_batted_in, req.body.home_runs, req.body.walks, req.body.strikeouts], function(err, results){
+    if(err){
+        if(err.code == 'ER_DUP_ENTRY'){
+            console.log(err.code);
+            mysql.pool.query("SELECT * FROM hitting_stats WHERE player_id=?", [req.body.player_id], function(err, result){
+                if(err){
+                next(err);
+                return;
+                }
+            
+                if(result.length == 1){
+                    var curVals = result[0];
+                    mysql.pool.query("UPDATE hitting_stats SET hits=?, batting_average=?, runs_scored=?, runs_batted_in=?, home_runs=?, walks=?, strikeouts=? WHERE player_id=? ",
+                        [req.body.hits || curVals.hits, req.body.batting_average || curVals.batting_average, req.body.runs_scored || curVals.runs_scored, req.body.runs_batted_in || curVals.runs_batted_in, 
+                        req.body.home_runs || curVals.home_runs, req.body.walks || curVals.walks, req.body.strikeouts || curVals.strikeouts, req.body.player_id],
+                        function(err, result){
+                            if(err){
+                            next(err);
+                            return;
+                            }
+
+                            else
+                            {
+                                res.redirect('/');                                        
+                            }
+                            
+                        });
+                 }
+            });
+        }
+                    
+        else{
+            next(err);
+            return;
+        }  
+        
+    }
+
+    else
+    {
+        res.redirect('/');                                        
+    }
+    });
+});
+
+
+
+app.post('/update-pitching-stats',function(req,res,next){
+  var context = {};
+
+  console.log("Updating stats for player: " + req.body.player_id);
+  
+  mysql.pool.query('INSERT INTO pitching_stats (`player_id`, `earned_run_average`, `opp_batting_average`, `wins`, `losses`, `walks`, `strikeouts`) VALUES (?, ?, ?, ?, ?, ?, ?)', [req.body.player_id, req.body.earned_run_average, req.body.opp_batting_average, req.body.wins, req.body.losses, req.body.walks, req.body.strikeouts], function(err, results){
+    if(err){
+        if(err.code == 'ER_DUP_ENTRY'){
+            console.log(err.code);
+  
+            mysql.pool.query("SELECT * FROM pitching_stats WHERE player_id=?", [req.body.player_id], function(err, result){
+                if(err){
+                    next(err);
+                    return;
+                }
+                
+                if(result.length == 1){
+                    var curVals = result[0];
+                    mysql.pool.query("UPDATE pitching_stats SET earned_run_average=?, opp_batting_average=?, wins=?, losses=?, walks=?, strikeouts=? WHERE player_id=? ",
+                        [req.body.earned_run_average || curVals.earned_run_average, req.body.opp_batting_average || curVals.opp_batting_average, req.body.wins || curVals.wins, req.body.losses || curVals.losses, 
+                        req.body.walks || curVals.walks, req.body.strikeouts || curVals.strikeouts, req.body.player_id],
+                        function(err, result){
+                            if(err){
+                                next(err);
+                                return;
+                            }
+                            else
+                            {
+                                res.redirect('/');
+                            }
+                         }); 
+                }  
+            });
+        }
+        
+        else{
+            next(err);
+            return;
+        } 
+    }
+
+    else
+    {
+        res.redirect('/');
+                
+    }
+    
+});
+});
+
+
+app.use(function(req,res){
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function(){
+  console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
+});
